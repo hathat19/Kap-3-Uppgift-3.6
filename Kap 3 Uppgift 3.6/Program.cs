2eb8876095270d239ba2adfd1dc3a3987a403f2b
humy06001 <humy06001@E-PW0234KS>
2023-09-29 11:25:15 +0200

    Add project files.

---
+++ Kap 3 Uppgift 3.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Uppgift3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+
+            Console.WriteLine("Vad heter du (För- och efternamn)?");
+            string fullName = Console.ReadLine();
+
+            int spaceIndex = fullName.IndexOf(" ");
+            string firstName = fullName[..spaceIndex];
+            string surname = fullName[(spaceIndex + 1)..];
+
+            if (firstName.CompareTo(surname) < 0)
+            {
+                Console.WriteLine($"{firstName} kommer först i bokstavsordning.");
+            }
+            else if (firstName.CompareTo(surname) > 0)
+            {
+                Console.WriteLine($"{surname} kommer först i bokstavsordning.");
+            }
+            else
+            {
+                Console.WriteLine("Du har samma förnamn som efternamn.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
